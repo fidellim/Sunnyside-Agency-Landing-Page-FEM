e4bed61f1a4be701caeb5053005b96cdbc58c364
fidellim <lim.fidel@gmail.com>
2021-10-21 16:26:02 +0400

    add gulp workflow

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,19 @@
 
 const { src, dest, watch, series } = require("gulp");
 const terser = require("gulp-terser");
+const sass = require("gulp-sass")(require("sass"));
+const postcss = require("gulp-postcss");
+const cssnano = require("cssnano");
+const autoprefixer = require("autoprefixer");
 const browsersync = require("browser-sync").create();
+
+// Sass Task
+function scssTask() {
+	return src("app/scss/styles.scss", { sourcemaps: true })
+		.pipe(sass())
+		.pipe(postcss([cssnano(), autoprefixer()]))
+		.pipe(dest("dist", { sourcemaps: "." }));
+}
 
 // Javascript Task
 function jsTask() {
@@ -30,8 +42,11 @@ function browsersyncReload(cb) {
 // Watch Task
 function watchTask() {
 	watch("*.html", browsersyncReload);
-	watch(["app/js/**/*.js"], series(jsTask, browsersyncReload));
+	watch(
+		["app/scss/**/*.scss", "app/js/**/*.js"],
+		series(scssTask, jsTask, browsersyncReload)
+	);
 }
 
 // Default Gulp Task
-exports.default = series(jsTask, browsersyncServe, watchTask);
+exports.default = series(scssTask, jsTask, browsersyncServe, watchTask);
